f2e5876073559d57fa472b9e2bc76ff65b2f6e3c
DOL2320 <dorian.majano@gmail.com>
2022-10-27 19:37:22 -0700

    Create project

---
+++ db/index.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('mydb', process.env.PG_USER, process.env.PG_Password, {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
